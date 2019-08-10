62ff92878172d1cfcc2bd1f908dc60ea75459bc1
“Nadiia <nadezhda277135@gmail.com>
2019-08-10 16:35:27 +0300

    Get attributes

---
+++ src/main/java/snippet/JsoupFindById.java
@@ -0,0 +1,54 @@
+package snippet;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.Optional;
+import java.util.stream.Collectors;
+
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+
+public class JsoupFindById {
+
+  private static Logger LOGGER = LoggerFactory.getLogger(JsoupFindById.class);
+
+  private static String CHARSET_NAME = "utf8";
+
+  public static void main(String[] args) {
+
+    // Jsoup requires an absolute file path to resolve possible relative paths in HTML,
+    // so providing InputStream through classpath resources is not a case
+    String resourcePath = "./startbootstrap-sb-admin-2-examples/sample-0-origin.html";
+    String targetElementId = "make-everything-ok-button";
+
+    Optional<Element> buttonOpt = findElementById(new File(resourcePath), targetElementId);
+
+    Optional<String> stringifiedAttributesOpt = buttonOpt.map(button ->
+        button.attributes().asList().stream()
+            .map(attr -> attr.getKey() + " = " + attr.getValue())
+            .collect(Collectors.joining(", "))
+    );
+
+    stringifiedAttributesOpt.ifPresent(attrs -> LOGGER.info("Target element attrs: [{}]", attrs));
+  }
+
+  private static Optional<Element> findElementById(File htmlFile, String targetElementId) {
+    try {
+      Document doc = Jsoup.parse(
+          htmlFile,
+          CHARSET_NAME,
+          htmlFile.getAbsolutePath());
+
+      return Optional.of(doc.getElementById(targetElementId));
+
+    } catch (IOException e) {
+      LOGGER.error("Error reading [{}] file", htmlFile.getAbsolutePath(), e);
+      return Optional.empty();
+    }
+  }
+
+}
